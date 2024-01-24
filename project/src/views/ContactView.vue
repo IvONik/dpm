31f64f6b7d91799e7fc907ac8bf65a3a8a8ed84d
Olga <ola.noskova@mail.ru>
2024-01-24 23:45:58 +0400

    change map

--- project/src/views/ContactView.vue
+++ project/src/views/ContactView.vue
@@ -2,7 +2,17 @@
   <div>
     <NavComp />
     <div class="title">Наши контакты</div>
-    <div id="map"></div>
+    <div class="container">
+
+      <div id="map"></div>
+      <ul class="right__part">
+        <li v-for="item in addresses" :key="item.id" class="right__part__item">
+          {{ item.address }}<br>
+          {{ item.worksTime }}
+        </li>
+      </ul>
+
+    </div>
 
   </div>
 </template>
@@ -14,53 +24,105 @@
 
 import NavComp from '@/components/NavComp.vue';
 
-export default {
-  name: 'DiplomContactView',
-  async created() {
-    try {
-      let map = document.getElementById("map")
 
-      // let map;
-      async function initMap() {
-        const position = { lat: 59.916, lng: 30.326 };
+export default {
+  data() {
+    return {
+      addresses: [
+        {
+          id: 1,
+          address: 'ул. Константина Заслонова, 2',
+          worksTime: 'пн-пт: 9:00 - 20:00, сб-вс: по записи',
+        },
+        {
+          id: 2,
+          address: 'Клинский проспект, 5/17',
+          worksTime: 'пн-пт: 9:00 - 20:00, \n сб-вс: по записи',
+        }
+      ],
+    };
+  },
+  name: 'DiplomContactView',
+  
+  components: { NavComp },
+  created() {
+    this.initMap();
+  },
+  methods: {
+    async initMap() {
+      try {
+        let map;
         const { Map } = await google.maps.importLibrary("maps");
         const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+        let centerMap = new google.maps.LatLng(59.919, 30.339);
 
         map = new Map(document.getElementById("map"), {
-          zoom: 16,
-          center: position,
+          zoom: 13,
+          center: centerMap,
           mapId: "DEMO_MAP_ID",
         });
-        const marker = new AdvancedMarkerView({
-          map: map,
-          position: position,
-          title: "Uluru",
+
+        this.addresses.forEach((address) => {
+          let position;
+          if (address.id === 1) {
+            position = { lat: 59.920, lng: 30.352 };
+          } if (address.id === 2) {
+            position = { lat: 59.91627, lng: 30.32677 };
+          }
+
+          const contentString = `<p>${address.address}</p><p>${address.worksTime}</p>`;
+          const infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            ariaLabel: "Address Info",
+          });
+
+          const marker = new AdvancedMarkerView({
+            map: map,
+            position: position,
+            title: 'Стоматология',
+          });
+
+          marker.addListener("click", () => {
+            infowindow.open({
+              anchor: marker,
+              map,
+            });
+          });
         });
+      } catch (error) {
+        console.error('Ошибка инициализации карты:', error.message);
       }
-      // document.addEventListener("DOMContentLoaded", function () {
-      //   initMap();
-      // });
-      initMap();
-    } catch (err) {
-      console.log(err);
     }
-  },
-  components: { NavComp },
-
+  }  
 };
-// document.addEventListener("DOMContentLoaded", function() {
-//   initMap();
-// });
-
 
 
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/style/_vars.scss';
+
+.container {
+  display: flex;
+  justify-content: space-around;
+
+}
+
+.right__part {
+  width: 30%;
+  padding-top: 30px;
+  gap: 25px;
+  color: $colorText;
+  font-size: 20px;
+
+  &__item {
+    margin-bottom: 20px;
+  }
+}
+
 #map {
   width: 60%;
   height: 400px;
   border-radius: 20px;
   border: 1px solid #141618;
-}
-</style>
+}</style>
